3734f960d828d679cdcd4fc8bd9cd59ff1030dd5
Tatum <tatum@ashlandstudios.com>
2026-02-09 10:18:27 -0500

    WIP

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,8 +13,4 @@ class ApplicationController < ActionController::API
       same_site: :strict
     }
   end
-
-  def verified_request?
-    super || valid_authenticity_token?(session, request.headers["X-XSRF-TOKEN"])
-  end
 end
--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -11,7 +11,7 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(
       withXsrfConfiguration({
         cookieName: 'XSRF-TOKEN',
-        headerName: 'X-XSRF-TOKEN'
+        headerName: 'X-CSRF-Token'
       })
     )
   ]
